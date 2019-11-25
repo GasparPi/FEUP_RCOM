6a5fe729102cad5ce67ec0de37df0086044f6ed5
Marantesss <up201706917@fe.up.pt>
2019-11-25 10:18:55 +0000

    Added eth1 up for tux54 + reorganized file structure

--- Proj2/scripts/tux52.sh
+++ Proj2/scripts/tux52.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # this script is meant to be executed at the
-# beginning of every RCOM TP class to configure
+# beginning of every RCOM TP class to set up
 # each computers ethernet configuration
 
 # activate tux51
 ifconfig eth0 up
 
-# configure eth0 with address 192.168.51.1 and with 24 bits mask
+# configure eth0 with address 192.168.51.1 and with 24 bits mask (sub-network 51)
 ifconfig eth0 192.168.51.1/24
--- Proj2/scripts/tux54.sh
+++ Proj2/scripts/tux54.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # this script is meant to be executed at the
-# beginning of every RCOM TP class to configure
+# beginning of every RCOM TP class to set up
 # each computers ethernet configuration
 
-# activate tux51
+# activate tux54 ethernet port 0
 ifconfig eth0 up
 
-# configure eth0 with address 192.168.50.254 and with 24 bits mask
+# configure eth0 with address 192.168.50.254 and with 24 bits mask (sub-network 50)
 ifconfig eth0 192.168.50.254/24
 
-# configure eth1 with address 192.168.51.253 and with 24 bits mask
+# activate tux54 ethernet port 1
+ifconfig eth1 up
+
+# configure eth1 with address 192.168.51.253 and with 24 bits mask (sub-network 51)
 ifconfig eth1 192.168.51.253/24
